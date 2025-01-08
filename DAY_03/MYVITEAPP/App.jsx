8a8acacaaff01347c84b270aef7b9048d184c7e9
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-08 16:02:37 +0530

    Create App.jsx

---
+++ DAY_03/MYVITEAPP/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Footer from "./Footer"
+import Header from "./Header"
+import Content from "./Content"
+import {Component1} from "./Component";
+import {Component2} from "./Component";
+import {Component3} from "./Component";
+import {Component4} from "./Component";
+import {Component5} from "./Component";
+
+function App() {
+  return (
+      <>
+          <Header />
+          <Content />
+          <Footer />
+          <Component1 />
+          <Component2 />
+          <Component3 />
+          <Component4 />
+          <Component5 />
+      </>
+  );
+}
+
+
+export default App;
