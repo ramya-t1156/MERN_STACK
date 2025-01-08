67c60eb3897a76f772c7cb6bcd57f8d5e32db2dc
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-08 16:03:16 +0530

    Create Content.jsx

---
+++ DAY_03/MYVITEAPP/Content.jsx
@@ -0,0 +1,16 @@
+const Content = () => {
+    return (
+        <div>
+        <div>
+            <h1>Hello World</h1>
+            <ul>
+                <li>content-01</li>
+                <li>content-02</li>
+                <li>content-03</li>
+            </ul>
+        </div>    
+        </div>
+    );
+};
+
+export default Content;
