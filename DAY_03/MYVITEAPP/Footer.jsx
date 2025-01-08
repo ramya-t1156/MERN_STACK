b7312358ce5c920c0e3734ed378c3b9d5c0631cb
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-08 16:01:53 +0530

    Create Footer.jsx

---
+++ DAY_03/MYVITEAPP/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <div>
+        <div>
+            <ul>
+                <li>footer-01</li>
+                <li>footer-02</li>
+                <li>footer-03</li>
+            </ul>
+        </div>    
+        <div>
+            <h3>Thank you!!</h3>
+        </div>
+        </div>
+    );
+};
+
+export default Footer;
