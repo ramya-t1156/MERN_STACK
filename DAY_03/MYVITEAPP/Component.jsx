fe832b7168f1ae4e570552665f84cf5ced0baac5
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-08 16:02:56 +0530

    Create Component.jsx

---
+++ DAY_03/MYVITEAPP/Component.jsx
@@ -0,0 +1,16 @@
+//Avoid using multiple default exports in a single file.
+export function Component1() {
+    return <div>Component-1</div>;
+}
+export function Component2() {
+    return <div>Component-2</div>;
+}
+export function Component3() {
+    return <div>Component-3</div>;
+}
+export function Component4() {
+    return <div>Component-4</div>;
+}
+export function Component5() {
+    return <div>Component-5</div>;
+}
