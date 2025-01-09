f6fa124ddb209e71cd721f5ccde233222e2ccfa4
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-09 15:11:58 +0530

    Create ContextApi.jsx

---
+++ DAY_04/HOOKS/useEffect/ContextApi.jsx
@@ -0,0 +1,8 @@
+// step 1 : create file - ContextApi
+// import the function from react
+import{createContext} from "react";
+// step 2 : craete context
+// create our own file
+const MyContext = createContext();
+export default MyContext;
+// move to root(main.jsx)
