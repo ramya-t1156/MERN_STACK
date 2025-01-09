2e1547eb274a36911220996e98c863249079144e
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-09 10:53:51 +0530

    Update Content.jsx

--- DAY_04/EVENTS/Content.jsx
+++ DAY_04/EVENTS/Content.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
 
+// const Content = () => {
+//     return (
+//         <div>
+//             <button>Click me</button>
+//         </div>
+//     );
+// };
 
 function Content(){
-    function handleClick(){
-        alert("you clicked the button");
-    }
-    return (
-        <div>
-            <button onClick = {handleClick}>Click me</button>
-            {/* <button onClick = {handleClick()}>Click me</button> // the function will called before the button clicked because of ()->this symbol*/}
+    // //example -1 
+    // function handleClick(){
+    //     alert("you clicked the button");
+    // }
+    // return (
+    //     <div>
+    //         <button onClick = {handleClick}>Click me</button>
+    //         {/* <button onClick = {handleClick()}>Click me</button> // the function will called before the button clicked because of ()->this symbol
+    //         // the event will be immediatly triggered*/}
             
-        </div>
-    );
-}
+    //     </div>
+    // );
 
+    // //example -2
+    // //callinga n function with parameters 
+    // function handleClick(msg){
+    //     alert(msg);
+    // }
+    // return (
+    //     <div>
+    //         <button onClick = {()=>handleClick("you clicked the button")}>Click me</button>
+    //     </div>
+    // );
+
+    
+}
 
 export default Content;
