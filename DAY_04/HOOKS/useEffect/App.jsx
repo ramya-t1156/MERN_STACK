41fbc8ce58062c86d9152223aa1687a3c87ec948
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-09 15:13:27 +0530

    Create App.jsx

---
+++ DAY_04/HOOKS/useEffect/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from "./Footer"
+import Header from "./Header"
+import Content from "./Content"
+import ApiCall from './ApiCall';
+
+function App() {
+  return (
+      <>
+          <ApiCall/>
+      </>
+  );
+}
+
+
+export default App;
