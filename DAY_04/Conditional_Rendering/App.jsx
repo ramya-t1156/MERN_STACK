31692cc4de85171f3c39b98a3c0a2dd872562d0c
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-09 10:49:21 +0530

    Create App.jsx

---
+++ DAY_04/Conditional_Rendering/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from "./Footer"
+import Header from "./Header"
+import Content from "./Content"
+
+function App() {
+  return (
+      <>
+          <Content/>
+      </>
+  );
+}
+
+
+export default App;
