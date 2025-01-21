6cb831e99cd319552d6fc55bec264073e6d757cb
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-21 23:12:46 +0530

    Create App.jsx

---
+++ DAY_05/static_routing/App.jsx
@@ -0,0 +1,15 @@
+// Definition: Predefined routes that remain fixed and are defined in advance. 
+// The routes don't change dynamically during runtime.
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
