df03faeca5b69e0cbfb1720e53df7aa93099f572
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-21 22:42:56 +0530

    Create useReducer.jsx

---
+++ DAY_04/HOOKS/useReducer.jsx
@@ -0,0 +1,25 @@
+// An alternative to useState for managing complex state logic using a reducer.
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+}
+
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  return (
+    <div>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </div>
+  );
+}
