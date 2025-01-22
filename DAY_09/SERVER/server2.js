81692f90935c6369bac97f9aa15778cfa04dbbd9
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2025-01-22 19:16:55 +0530

    Update server2.js

--- DAY_09/SERVER/server2.js
+++ DAY_09/SERVER/server2.js
@@ -1,6 +1,27 @@
+simple
+
+ex - 01
 const http = require("http")
+const server = http.createServer((req,res)=>{
+    console.log(req.url)
+    res.end("hello")
+});
+
+server.listen(3000,()=>{console.log("heyy")});
+
+
+ex - 02
+http://localhost:3000/?id=10&name=jhon
+
+const http = require("http")
+const url = require("url")
 
 const server = http.createServer((req,res)=>{
-    res.end("hello")
-})
-server.listen(1000,()=>{console.log("hey")})
+    let {query} = url.parse(req.url,true) // true - whether our req contains url
+    console.log(query.id)
+    let{id,name} = query
+    console.log(id)
+    console.log(name)
+});
+
+server.listen(3000,()=>{console.log("heyy")});
